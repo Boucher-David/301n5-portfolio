15a8d33cc98a922372e2536b94289f1d0ec0b392
David Boucher <david.boucher@ookla.com>
2017-07-29 22:32:34 -0700

    Add Work Experience and Append to Section

--- dataConstructor.js
+++
@@ -1,9 +0,0 @@
-// Sample JS Constructor
-
-function projectData(tab, name, mySkills, projectLinks, socialLink) {
-  this.tab = tab;
-  this.projectName = name;
-  this.mySkills = mySkills;
-  this.projectLinks = projectLinks;
-  this.social = socialLink;
-}
---
+++ scripts/workExperience.js
@@ -0,0 +1,19 @@
+var workExperience = [{
+  company: 'Ookla',
+  duration: '18 Months',
+  title: 'Ad Operations Specialist',
+  roles: [],
+  companyURL: 'www.ookla.com'
+},{
+  company: 'CDK Global',
+  duration: '6 Months',
+  title: 'Ad Operations Specialist',
+  roles: [],
+  companyURL: 'www.cdk.com'
+},{
+  company: 'Pentwyn Post Office',
+  duration: '2 Years',
+  title: 'Temporary',
+  roles: [],
+  companyURL: 'n/a'
+}];
