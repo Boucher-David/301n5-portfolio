125efe2cf11a65c4b46450e836537994b84b66d9
David Boucher <david.boucher@ookla.com>
2017-08-15 20:03:26 -0700

    IIFE Loader of Templates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,33 @@
-// replace this with IIFE that gets all templates and precompiles them.It should also potentially get all data and put them together too.
-// ()=> {}()
-// can also write it using just {}
-function getHBTemplate(tab) {
-  $.getJSON(`scripts/templateJSON/${tab}.json`).done((json) => {
-    $.get(`scripts/templateRaw/${tab}.hbs`, (t) => {
-      let template = Handlebars.compile(t);
-      $('.main-content').html(template(json));
+let templates = [];
+
+// IIFE that loads templates for quick loading
+(function() {
+  ['home','about','portfolio','social'].forEach((tab) => {
+    $.getJSON(`scripts/templateJSON/${tab}.json`).done((json) => {
+      $.get(`scripts/templateRaw/${tab}.hbs`, (t) => {
+        let template = Handlebars.compile(t);
+        templates[`${tab}SVG`] = template(json);
+        if (tab === 'home') {$('.main-content').html(templates['homeSVG']); }
+      });
     });
   });
-}
+})();
+
 
 // Load home on page load
-getHBTemplate('home');
 
 // Nav clicking. Clicking on a nav loads a different handlebars template into main content div
 $('.svg-parent svg').on('click', function(e) {
   e.preventDefault();
-  let clickedSVG = $(this).attr('id').replace('SVG', '');
 
   // load template based on nav selected
-  getHBTemplate(clickedSVG);
+  $('.main-content').html(templates[$(this).attr('id')]);
 
   // Hide all navs except home.
   $('.svg-parent svg').not('#homeSVG').hide();
 
   // clicking on home should show nav bar again.
-  if (clickedSVG === 'home' && $('.svg-parent svg').not('#homeSVG').not(':visible')) {
+  if ($(this).attr('id') === 'homeSVG' && $('.svg-parent svg').not('#homeSVG').not(':visible')) {
     $('.svg-parent svg').not('#homeSVG').show();
   }
 
